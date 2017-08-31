9d0a948dd7a7800576a2d25916d46aff3ed4758e
Aidan Sawyer <aidan.sawyer@ithaka.org>
2017-08-30 21:52:05 -0400

    very minor refactor (comments, generate_filename)

--- build_resume.py
+++ build_resume.py
@@ -10,11 +10,10 @@ from build.update_values_helpers import *
 
 def generate_filename(last_name=""):
     date = time.strftime("%Y-%m-%d")
-    filename = "Resume_{name}{date}".format(
+    return "Resume_{name}{date}".format(
         name=last_name+"_" if last_name else "",
-        date=str(date)
+        date=date
     )
-    return filename
 
 
 def sanitize_latex_syntax(line):
@@ -22,29 +21,28 @@ def sanitize_latex_syntax(line):
 
 
 def update_values(dict_values):
-    # add header
-    generate_header_info(dict_values)
 
-    # add objective
+    # header and objective
+    generate_header_info(dict_values)
     dict_values.update({
         "OBJECTIVE":"I'm looking for meaningful work involved with the organization, preservation, and expansion of knowledge, with particular regard to library resources and scientific research. By improving the engines which power human understanding, I seek to expedite human progress long-term."
     })
 
-    # add education
+    # education
     educations = json.load(open('education.json'))
     generate_school_info(dict_values, educations[0])
 
-    # add work experience
+    # work experience
     experiences = json.load(open('experience.json'))
     for i, work_experience in enumerate(experiences[:3], start=1):
         generate_work_experience(dict_values, work_experience, i)
 
-    # add projects
+    # projects
     projects = json.load(open('projects.json'))
     for i, project in enumerate(projects[:3], start=1):
         generate_project(dict_values, project, i)
 
-    # add languages
+    # languages
     additional = json.load(open('additional.json'))
     languages = additional['languages']
     generate_languages(dict_values, languages)
@@ -66,7 +64,7 @@ if __name__ == "__main__":
     # copy .tex template into a new (temporary) file 'filename.tex'
     copyfile(tex_template_filepath, tex_new_filepath)
 
-    # use `dict_values` to replace placeholders with real values in filename.tex
+    # use `dict_values` to replace placeholders in template with real values in the new one
     resume_template = open(tex_template_filepath, 'r')
     output_resume = open(tex_new_filepath, 'w')
     for line in resume_template:
