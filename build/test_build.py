fbf8e52a172658bbbd63fff4bd4e470a0ef0e54d
atla5 <aks5238@rit.edu>
2017-10-31 19:53:14 -0400

    test: test_valid_csv_data() in data_dir

--- build/test_build.py
+++ build/test_build.py
@@ -1,5 +1,5 @@
 import unittest
-import os, json
+import os, json, csv
 from update_values_helpers import *
 
 
@@ -48,3 +48,14 @@ class TestBuild(unittest.TestCase):
                 except ValueError:
                     self.fail("linting error found in {}".format(filename))
 
+    def test_valid_csv_data(self):
+        for filename in os.listdir(data_dir):
+            if filename.endswith('.csv'):
+                try:
+                    with open(os.path.join(data_dir, filename), 'r') as csvfile:
+                        for row in csv.reader(csvfile, delimiter=','):
+                            if not bool(row) or not isinstance(row, list) or not all(isinstance(elem, str) for elem in row):
+                                self.fail("error rendering csv file '{}' into list of strings.".format(filename))
+
+                except Exception:
+                    self.fail("error reading csv file: {}".format(filename))
